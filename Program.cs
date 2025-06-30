eedb37a4d75cc2b7ab1c9ad521d3bc1b5d532d1f
結友 <miagetegorann@gmail.com>
2025-07-01 00:58:17 +0900

    fix

--- Program.cs
+++ Program.cs
@@ -12,26 +12,10 @@ builder.Services.AddHttpClient("OpenAI", client =>
     client.BaseAddress = new Uri("https://api.openai.com/");
 });
 
-builder.Services.AddServerSideBlazor(options => {
-    options.DetailedErrors = true;
-    options.DisconnectedCircuitRetentionPeriod =
-        TimeSpan.FromMinutes(5);
-})
-.AddHubOptions(hubOptions => {
-    hubOptions.KeepAliveInterval = TimeSpan.FromSeconds(30);
-    hubOptions.ClientTimeoutInterval = TimeSpan.FromSeconds(120);
-});
-
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders =
         ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-});
-
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-    options.Secure = CookieSecurePolicy.Always;
 });
 
 var app = builder.Build();
@@ -42,16 +26,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseCookiePolicy();
-
 app.UseForwardedHeaders();
 
 app.UseAntiforgery();
 
 app.UseStaticFiles();
-app.UseRouting();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
 
 app.MapRazorComponents<App>()
    .AddInteractiveServerRenderMode();
