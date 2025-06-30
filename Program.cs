b83b5d234b96a10f81852c4224eeb99c12c5a59a
結友 <miagetegorann@gmail.com>
2025-06-30 23:59:36 +0900

    ログを吐くように変更

--- Program.cs
+++ Program.cs
@@ -12,6 +12,16 @@ builder.Services.AddHttpClient("OpenAI", client =>
     client.BaseAddress = new Uri("https://api.openai.com/");
 });
 
+builder.Services.AddServerSideBlazor(options => {
+    options.DetailedErrors = true;
+    options.DisconnectedCircuitRetentionPeriod =
+        TimeSpan.FromMinutes(5);
+})
+.AddHubOptions(hubOptions => {
+    hubOptions.KeepAliveInterval = TimeSpan.FromSeconds(30);
+    hubOptions.ClientTimeoutInterval = TimeSpan.FromSeconds(120);
+});
+
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders =
@@ -39,6 +49,9 @@ app.UseForwardedHeaders();
 app.UseAntiforgery();
 
 app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.MapRazorComponents<App>()
    .AddInteractiveServerRenderMode();
